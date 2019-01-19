12f008f1495f999eca1d84494a17ac505aac754a
Clyde Walter <cfwalter@Clydes-MBP.attlocal.net>
2019-01-19 14:35:05 -0500

    got express server started, serving static files

--- app/index.js
+++ app/index.js
@@ -1,11 +1,20 @@
 const express = require('express'),
       app = express(),
-      port = 11919;
+      port = 11919,
+      path = require('path'),
+      static = path.resolve(__dirname + '/../public/');
+
+// console.log("serve static files from", static);
+
+app.use(express.static(static));
+
+// console.log("resolve:", path.resolve(__dirname + '/../public/html/index.html'));
+// console.log("resolve:", path.resolve(__dirname + '/../public/html/index.html'));
 
 app.get('/', (request, response) => {
-  response.send('Welcome to the Monument.');
+  response.sendFile(path.resolve(__dirname + '/../public/html/index.html'));
 });
 
 app.listen(port, () => {
-  
-})
+  console.log(`The monument is listening to you on port ${port}.`);
+});
