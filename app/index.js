ea5af15df46e32f2f455030b7f137e19925df73b
Clyde Walter <cfwalter@Clydes-MacBook-Pro.local>
2019-01-20 00:15:12 -0500

    slight reorganization of index.js

--- app/index.js
+++ app/index.js
@@ -11,10 +11,10 @@ app.use(express.static(static));
 // console.log("resolve:", path.resolve(__dirname + '/../public/html/index.html'));
 // console.log("resolve:", path.resolve(__dirname + '/../public/html/index.html'));
 
-app.get('/', (request, response) => {
-  response.sendFile(path.resolve(__dirname + '/../public/html/index.html'));
-});
-
 app.listen(port, () => {
   console.log(`The monument is listening to you on port ${port}.`);
+});
+
+app.get('/', (request, response) => {
+  response.sendFile(path.resolve(__dirname + '/../public/html/index.html'));
 });
